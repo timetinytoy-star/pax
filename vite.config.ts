bba7e6dfff71f84d56a347ffa80c7ea4b98f0873
timetinytoy-star <timetinytoy@gmail.com>
2026-04-24 18:49:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -22,3 +22,11 @@ export default defineConfig(({mode}) => {
     },
   };
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Bunu eklemek, yolların "göreceli" olmasını sağlar
+})
